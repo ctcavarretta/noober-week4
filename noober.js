0f521fd2f9f942ff61afb553b791165d47ba2156
Christian Cavarretta <ctcavarretta@gmail.com>
2021-02-01 14:58:37 -0600

    commit

--- noober.js
+++ noober.js
@@ -6,7 +6,156 @@ async function pageLoaded() {
   console.dir(json)
   
   // 🔥 start here: write code to loop through the rides
-  
+  let levelOfService
+
+  for (let i=0; i<json.length; i++) {
+    console.log('Ride: ', json[i])
+    let ride = json[i]
+    for (let j=0; j<ride.length; j++) {
+      console.log('Leg: ', ride[j])
+      let leg = ride[j]
+      if(ride.length>1) {
+        levelOfService = 'Noober Pool'
+        document.querySelector('.rides').insertAdjacentHTML('beforeend', `
+        <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+          <i class="fas fa-car-side"></i>
+          <span>${levelOfService}</span>
+        </h1>
+        
+        <div class="border-4 border-gray-900 p-4 my-4 text-left">
+        <div class="flex">
+          <div class="w-1/2">
+            <h2 class="text-2xl py-1">${leg.passengerDetails.first} ${leg.passengerDetails.last}</h2>
+            <p class="font-bold text-gray-600">${leg.passengerDetails.phoneNumber}</p>
+          </div>
+          <div class="w-1/2 text-right">
+            <span class="rounded-xl bg-gray-600 text-white p-2">
+            ${leg.numberOfPassengers} passengers
+            </span>
+          </div>
+        </div>
+        <div class="mt-4 flex">
+          <div class="w-1/2">
+            <div class="text-sm font-bold text-gray-600">PICKUP</div>
+            <p>${leg.pickupLocation.address}</p>
+            <p>${leg.pickupLocation.city} ${leg.pickupLocation.state} ${leg.pickupLocation.zip}</p>
+          </div>
+          <div class="w-1/2">
+            <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+            <p>${leg.dropoffLocation.address}</p>
+            <p>${leg.dropoffLocation.city} ${leg.dropoffLocation.state} ${leg.dropoffLocation.zip}</p>
+            </div>
+          </div>
+        </div>
+      `)
+    }
+      else if(leg.purpleRequested == true) {
+        levelOfService = 'Noober Purple'
+        document.querySelector('.rides').insertAdjacentHTML('beforeend', `
+        <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+          <i class="fas fa-car-side"></i>
+          <span>${levelOfService}</span>
+       </h1>
+        
+       <div class="border-4 border-purple-500 p-4 my-4 text-left">
+        <div class="flex">
+          <div class="w-1/2">
+            <h2 class="text-2xl py-1">${leg.passengerDetails.first} ${leg.passengerDetails.last}</h2>
+            <p class="font-bold text-gray-600">${leg.passengerDetails.phoneNumber}</p>
+          </div>
+        <div class="w-1/2 text-right">
+          <span class="rounded-xl bg-purple-600 text-white p-2">
+          ${leg.numberOfPassengers} passengers
+          </span>
+        </div>
+        </div>
+          <div class="mt-4 flex">
+            <div class="w-1/2">
+             <div class="text-sm font-bold text-gray-600">PICKUP</div>
+              <p>${leg.pickupLocation.address}</p>
+              <p>${leg.pickupLocation.city} ${leg.pickupLocation.state} ${leg.pickupLocation.zip}</p>
+           </div>
+        <div class="w-1/2">
+          <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+           <p>${leg.dropoffLocation.address}</p>
+           <p>${leg.dropoffLocation.city} ${leg.dropoffLocation.state} ${leg.dropoffLocation.zip}</p>
+        </div>
+        </div>
+      </div>
+      `)  
+    } 
+      else if(leg.numberOfPassengers>3) {
+        levelOfService = 'Noober XL'
+        document.querySelector('.rides').insertAdjacentHTML('beforeend', `
+        <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+          <i class="fas fa-car-side"></i>
+          <span>${levelOfService}</span>
+        </h1>
+        
+        <div class="border-4 border-gray-900 p-4 my-4 text-left">
+        <div class="flex">
+          <div class="w-1/2">
+            <h2 class="text-2xl py-1">${leg.passengerDetails.first} ${leg.passengerDetails.last}</h2>
+            <p class="font-bold text-gray-600">${leg.passengerDetails.phoneNumber}</p>
+          </div>
+        <div class="w-1/2 text-right">
+          <span class="rounded-xl bg-gray-600 text-white p-2">
+          ${leg.numberOfPassengers} passengers
+          </span>
+        </div>
+        </div>
+          <div class="mt-4 flex">
+            <div class="w-1/2">
+             <div class="text-sm font-bold text-gray-600">PICKUP</div>
+              <p>${leg.pickupLocation.address}</p>
+              <p>${leg.pickupLocation.city} ${leg.pickupLocation.state} ${leg.pickupLocation.zip}</p>
+           </div>
+        <div class="w-1/2">
+          <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+           <p>${leg.dropoffLocation.address}</p>
+           <p>${leg.dropoffLocation.city} ${leg.dropoffLocation.state} ${leg.dropoffLocation.zip}</p>
+        </div>
+        </div>
+      </div>
+      `) 
+      }
+      else {
+        levelOfService = 'Noober X'
+        document.querySelector('.rides').insertAdjacentHTML('beforeend', `
+        <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+          <i class="fas fa-car-side"></i>
+          <span>${levelOfService}</span>
+        </h1>
+        
+        <div class="border-4 border-gray-900 p-4 my-4 text-left">
+        <div class="flex">
+          <div class="w-1/2">
+            <h2 class="text-2xl py-1">${leg.passengerDetails.first} ${leg.passengerDetails.last}</h2>
+            <p class="font-bold text-gray-600">${leg.passengerDetails.phoneNumber}</p>
+          </div>
+        <div class="w-1/2 text-right">
+          <span class="rounded-xl bg-gray-600 text-white p-2">
+          ${leg.numberOfPassengers} passengers
+          </span>
+        </div>
+        </div>
+          <div class="mt-4 flex">
+            <div class="w-1/2">
+             <div class="text-sm font-bold text-gray-600">PICKUP</div>
+              <p>${leg.pickupLocation.address}</p>
+              <p>${leg.pickupLocation.city} ${leg.pickupLocation.state} ${leg.pickupLocation.zip}</p>
+           </div>
+        <div class="w-1/2">
+          <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+           <p>${leg.dropoffLocation.address}</p>
+           <p>${leg.dropoffLocation.city} ${leg.dropoffLocation.state} ${leg.dropoffLocation.zip}</p>
+        </div>
+        </div>
+      </div>
+      `) 
+      }
+    }
+  } 
 }
 
 window.addEventListener('DOMContentLoaded', pageLoaded)
